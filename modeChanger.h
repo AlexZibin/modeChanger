a0bf9684e0b1c11cfe256b11d045e1a9b1881808
Alexander Zibin <37593964+AlexZibin@users.noreply.github.com>
2018-05-15 16:18:47 +0300

    Update modeChanger.h

--- modeChanger.h
+++ modeChanger.h
@@ -1,10 +1,9 @@
 #include "Timer.h" // https://github.com/AlexZibin/timer
 
 enum class LoopDir {FORWARD, BACK, FORWARD_AND_BACK, BACK_AND_FORWARD};
-//enum class returnValue {CONTINUE, FORWARD, BACK};
-//typedef returnValue (*fPtr)(long); 
-
-typedef int (*fPtr)(long); 
+enum class returnValue {CONTINUE, FORWARD, TERMINATE};
+typedef returnValue (*fPtr)(long); 
+//typedef int (*fPtr)(long); 
 
 class ModeChanger {
     fPtr *_funcArray;
@@ -23,11 +22,14 @@ class ModeChanger {
     int applyMode (fPtr newModeFunc);
     int callCurrModeFunc (long param);
     bool modeJustChanged (void);
+
+    // moves to next function only when current function returns returnValue::FORWARD
+    // returns true if all functions are executed of if current function returns returnValue::TERMINATE
     bool loopThruModeFunc (int nSec=10, int numCycles=1, LoopDir direction = LoopDir::FORWARD);
+    
     //bool loopThruModeFunc (int nSec=10, int numCycles=1, LoopDir direction = LoopDir::FORWARD, int startMode = 0);
     //bool loopThruModeFunc (int nSec, int numCycles, LoopDir direction, bool switchAtZero=false) {
     
-    // moves to next function only when current function returns zero:
     bool loopThruModeFunc (LoopDir direction = LoopDir::FORWARD, long numCycles=1); 
 };
 
